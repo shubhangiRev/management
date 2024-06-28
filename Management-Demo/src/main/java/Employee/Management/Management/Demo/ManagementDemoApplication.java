0d8f7fe2f570f76104649a99c94c05a5d189282c
Shubhangi <93246198+shubhangiRev@users.noreply.github.com>
2024-06-28 14:21:11 +0530

    Add files via upload

---
+++ Management-Demo/src/main/java/Employee/Management/Management/Demo/ManagementDemoApplication.java
@@ -0,0 +1,13 @@
+package Employee.Management.Management.Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManagementDemoApplication.class, args);
+	}
+
+}
---
+++ Management-Demo/src/test/java/Employee/Management/Management/Demo/ManagementDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Employee.Management.Management.Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
